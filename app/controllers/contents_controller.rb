6173b411a2a064e05161c6bb5635691e9dbe8da2
kento.okamoto <mntymnty6@gmail.com>
2022-11-20 08:01:51 +0000

    フォームと一覧表示

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,6 @@
 class ContentsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:update, :destroy]
 
   def create
     # content = User.first.contents.build(item_id: 24, amount: 10)
@@ -10,19 +10,27 @@ class ContentsController < ApplicationController
       redirect_to root_url
     else
       flash.now[:danger] = "登録に失敗しました。"
+      render 'toppages/index'
     end
   end
 
   def update
+    flash[:success] = "update#{@content.item.name}"
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @content.destroy
+    flash[:success] = "#{@content.item.name}を削除しました。"
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def content_params
-    params.require(:content).permit(:item, :amount)
+    params.require(:content).permit(:item_id, :amount)
+    #TODO その他の分岐
+    # [Item.new(category_id: 1, is_approved: false)]
   end
   
   def correct_user
--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,18 +1,18 @@
 module ContentsHelper
-    def category_pulldown_list
-        list = []
+    def category_pulldown_hash
+        hash = {}
         Category.all.each do |c|
-            list.push(c.name)
+            hash.store(c.name, c.id)
         end
-        list
+        hash
     end
 
-    def item_pulldown_list(category_id)
-        list = []
+    def item_pulldown_hash(category_id)
+        hash = {}
         Item.all.where(category_id: category_id).each do |i|
-            list.push(i.name)
+            hash.store(i.name, i.id)
         end
-        # [Item.new(category_id: 1, is_approved: false)]
-        list.push('その他')
+        hash.store('その他', nil)
+        hash
     end
 end
